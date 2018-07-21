6c5916dff173a31e347cba01057b3b057d0e059f
KirylNik <ultramylo@gmail.com>
2018-07-21 15:46:50 +0300

    Feature: The list of articles has been transferred to the store.

---
+++ react-study/HT5/src/reducer/articles.js
@@ -0,0 +1,19 @@
+import {normalizedArticles} from '../fixtures'
+import {DELETE_ARTICLE} from '../constants'
+console.dir(normalizedArticles);
+const articlesMap = normalizedArticles.reduce((acc, articles) => {
+    acc[articles.id] = articles
+    return acc
+}, {})
+
+const articleArr = Object.values(articlesMap);
+
+export default (articlesState = articleArr, action) => {
+    const {type, payload} = action
+
+    switch (type) {
+        case DELETE_ARTICLE: return articleState.filter(article => article.id !== payload.id)
+    }
+
+    return articlesState
+}
